98bbfc25ac18ad2189d2e0210143c11fb1a5c894
pawel <pawel.kuznik@gmail.com>
2018-10-15 23:36:34 +0200

    Initial commit. Contains the EventHandlers, Component and the List classes.

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ *  This is a file that exports the public interface of the library.
+ *
+ *  @author     Pawel Kuznik <pawel.kuznik@gmail.com>
+ */
+
+// this is the public interface of the library
+module.exports = {
+    Component: require('./lib/Component.js'),
+    List: require('./lib/List.js')
+};
