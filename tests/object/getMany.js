5af68cc11c97d00ea3a3b13404c5b591249b2628
pawel <pawel.kuznik@gmail.com>
2019-01-24 20:49:58 +0100

    Implemented the object.getMany() function

--- tests/object/getMany.js
+++ tests/object/getMany.js
@@ -15,10 +15,24 @@ describe('object.getMany()', () => {
         let obj = { foo: 'a', baz: 'b' };
 
         // flatten the object
-        let result = sparkle.object.getMany();
+        let result = sparkle.object.getMany(obj);
 
         // check if both propertie are there
         expect(result).to.have.property('foo').that.is.equal('a');
         expect(result).to.have.property('baz').that.is.equal('b');
+    });
+
+    // we want to check if this function works for deep objects
+    it('should work on a deep object', () => {
+    
+        // a deep object
+        let obj = { foo: { baz: 'a' }, a: { b: { c: 'b' } } };
+
+        // flatten the object
+        let result= sparkle.object.getMany(obj);
+
+        // check if the deep property was recognized
+        expect(result).to.have.property('foo.baz').that.is.equal('a');
+        expect(result).to.have.property('a.b.c').that.is.equal('b');
     });
 });
