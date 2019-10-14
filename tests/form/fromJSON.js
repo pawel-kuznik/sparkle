9df6c7fb789f29cf6adc846adbb2258f825c658f
pawel <pawel.kuznik@gmail.com>
2019-10-14 09:46:17 +0200

    {auto} Improved test script to cover more cases (we need more)

--- tests/form/fromJSON.js
+++ tests/form/fromJSON.js
@@ -25,19 +25,51 @@ describe('form.toJSON()', () => {
     });
 
     // the function to handle NULL correctly
-    it('should fill NULL as empty space', () => {
+    it('should fill NULL as empty string on text inputs and textares', () => {
 
         // create a form element
         let form = document.createElement('FORM');
+
+        // create a text input
         let input = document.createElement('INPUT');
         input.setAttribute('type', 'text');
         input.setAttribute('name', 'foo');
         form.appendChild(input);
 
+        // create a textarea
+        let textarea = document.createElement('TEXTAREA');
+        textarea.setAttribute('name', 'textfoo');
+        form.appendChild(textarea);
+
         // fill in the form
-        sparkle.form.fromJSON(form, { foo: null });
+        sparkle.form.fromJSON(form, {
+            foo: null,
+            textfoo: null
+        });
 
         // check if the value is there
         expect(input.value).to.equal('');
+        expect(textarea.value).to.equal('');
+    });
+
+    it('should fill flag checkboxes', () => {
+
+        // creat a form element
+        let form = document.createElement('FORM');
+        let input = document.createElement('INPUT');
+        input.setAttribute('type', 'checkbox');
+        input.setAttribute('name', 'foo');
+
+        // fill the form
+        sparkle.from.fromJSON(form, { foo: true });
+
+        // check if the checkbox is properly set
+        expect(input.checked).to.equal(true);
+
+        // fill the form with opposise value
+        sparkle.from.fromJSON(form, { foo: false });
+
+        // check if the value is properly set
+        expect(input.checked).to.equal(false);
     });
 });
