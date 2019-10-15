77d90c8d2ecb7c546ccefb71612ace40f929f686
pawel <pawel.kuznik@gmail.com>
2019-10-15 23:02:59 +0200

    I started on custom form serialization. We are able to multiple handle checkboxes.

--- tests/form/toJSON.js
+++ tests/form/toJSON.js
@@ -17,7 +17,7 @@ describe('form.toJSON()', () => {
         expect(sparkle.form.toJSON(form)).to.be.an('object');
     });
 
-    it('should onlu parse inputs with name', () => {
+    it('should only parse inputs with name', () => {
 
         // create a form element
         let form = document.createElement('FORM');
@@ -79,5 +79,44 @@ describe('form.toJSON()', () => {
 
         // expect true value from a lone checkbox
         expect(data).to.have.property('test').that.equals(true);
+    });
+
+    it('should handle multiple checkboxes', () => {
+
+        // create a form elemnt
+        let form = document.createElement('FORM');
+
+        // create a checkbox
+        let checkbox1 = document.createElement('INPUT');
+        checkbox1.setAttribute('type', 'checkbox');
+        checkbox1.setAttribute('name', 'foo');
+        checkbox1.checked = true;
+        checkbox1.value = 'baz1';
+
+        form.append(checkbox1);
+
+        // create a checkbox
+        let checkbox2 = document.createElement('INPUT');
+        checkbox2.setAttribute('type', 'checkbox');
+        checkbox2.setAttribute('name', 'foo');
+        checkbox2.checked = true;
+        checkbox2.value = 'baz2';
+
+        form.append(checkbox2);
+
+        // create a checkbox
+        let checkbox3 = document.createElement('INPUT');
+        checkbox3.setAttribute('type', 'checkbox');
+        checkbox3.setAttribute('name', 'foo');
+        checkbox3.checked = false;
+        checkbox3.value = 'baz3';
+
+        form.append(checkbox3);
+
+        // get the data
+        const data = sparkle.form.toJSON(form);
+
+        // make sure we have only baz1 and baz2 in output data
+        expect(data).to.have.property('foo').that.deep.equal(['baz1', 'baz2']);
     });
 });
