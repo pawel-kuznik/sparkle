4595f5cd0b0d55230d9872438455e5fb20dde7bf
pawel <pawel.kuznik@gmail.com>
2019-10-23 20:27:18 +0200

    Implemented property the form.fromJSON() method.

--- tests/form/fromJSON.js
+++ tests/form/fromJSON.js
@@ -52,6 +52,30 @@ describe('form.fromJSON()', () => {
         expect(textarea.value).to.equal('');
     });
 
+    it('should fill select values', () => {
+
+        let form = document.createElement('FORM');
+        let input = document.createElement('SELECT');
+        input.setAttribute('name', 'test');
+        form.append(input);
+
+        let option1 = document.createElement('OPTION');
+        option1.setAttribute('value', 'foo1');
+        input.append(option1);
+
+        let option2 = document.createElement('OPTION');
+        option2.setAttribute('value', 'foo2');
+        input.append(option2);
+
+        // fill the form
+        sparkle.form.fromJSON(form, {
+            test: 'foo2'
+        });
+
+        // check if second option is selected
+        expect(option2.selected).to.equal(true);
+    });
+
     it('should fill flag checkboxes', () => {
 
         // creat a form element
@@ -59,17 +83,49 @@ describe('form.fromJSON()', () => {
         let input = document.createElement('INPUT');
         input.setAttribute('type', 'checkbox');
         input.setAttribute('name', 'foo');
+        form.append(input);
 
         // fill the form
-        sparkle.from.fromJSON(form, { foo: true });
+        sparkle.form.fromJSON(form, { foo: true });
 
         // check if the checkbox is properly set
         expect(input.checked).to.equal(true);
 
         // fill the form with opposise value
-        sparkle.from.fromJSON(form, { foo: false });
+        sparkle.form.fromJSON(form, { foo: false });
 
         // check if the value is properly set
         expect(input.checked).to.equal(false);
+    });
+
+    it('should fill checkboxes with an array', () => {
+
+        // creat a form element
+        let form = document.createElement('FORM');
+        let input = document.createElement('INPUT');
+        input.setAttribute('type', 'checkbox');
+        input.setAttribute('name', 'foo');
+        input.value = 'fooval';
+        form.append(input);
+
+        let another = document.createElement('INPUT');
+        another.setAttribute('type', 'checkbox');
+        another.setAttribute('name', 'foo');
+        another.value = 'anotherval';
+        form.append(another);
+
+        // fill the form
+        sparkle.form.fromJSON(form, { foo: [ 'fooval' ] });
+
+        // check if the checkbox is properly set
+        expect(input.checked).to.equal(true);
+        expect(another.checked).to.equal(false);
+
+        // fill the form with opposise value
+        sparkle.form.fromJSON(form, { foo: ['anotherval'] });
+
+        // check if the value is properly set
+        expect(input.checked).to.equal(false);
+        expect(another.checked).to.equal(true);
     });
 });
